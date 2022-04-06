4512f0215022172812e6414681841bbf63da596a
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-04-06 11:21:42 -0300

    Initial commit

---
+++ src/main/java/com/trybe/acc/java/planejamentodeviagem/Voo.java
@@ -0,0 +1,24 @@
+package com.trybe.acc.java.planejamentodeviagem;
+
+public class Voo {
+  private int tempoVoo;
+
+  /**
+   * Método para retornar tempo de Voo.
+   * 
+   */
+  public int retornarTempoVoo(double distanciaKm) {
+
+    Double duracaoDouble = distanciaKm / 700.0;
+
+  }
+
+  /**
+   * Método para retornar informaçao do Voo.
+   * 
+   */
+  public String retornarInformacaoVoo(String embarque, String origem, String desembarque,
+      String destino) {
+
+  }
+}
